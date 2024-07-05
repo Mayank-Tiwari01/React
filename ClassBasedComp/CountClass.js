4dedcf0c20371d2a5d091885fd899829a8bf8577
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-07-06 03:11:50 +0530

    learning class based react components.

---
+++ ClassBasedComp/CountClass.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class CountClass extends React.Component {
+  constructor(props) {
+    super(props);
+    // Initializing state
+    this.state = {
+      count: 0,
+      str: "Class based learning"
+    };
+    // Binding the handleClick method to the class instance
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // Method to handle click event
+  handleClick() {
+    // Incorrect way to update state
+    // this.setState(this.state.count + 1);
+    // Correct way to update state
+    this.setState(() => ({ count: this.state.count + 1000999990000 }));
+  }
+
+  // Render method to display the component
+  render() {
+    return (
+      <>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.count}</p>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.handleClick}>Click me as usual</button>
+      </>
+    );
+  }
+}
+
+export default CountClass;
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Timer from "./Hooks/UseEffect";
 import Nasa from "./components/Nasa";
+import CountClass from "./ClassBasedComp/CountClass";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Nasa/>);
+root.render(<CountClass  name="mayank" count="23" />);
